b7c6ae867aafbf7ea5b6871bca4b02eda30b55c6
Dylan Rhoads <Dhunterr1776@gmail.com>
2026-01-12 16:17:52 -0700

    ready

--- Week01/scripts/getdates.js
+++ Week01/scripts/getdates.js
@@ -2,3 +2,5 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+// Scripts
