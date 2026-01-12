248c1e7508304647d84f807f1cb00f516d0bc570
Dylan Rhoads <Dhunterr1776@gmail.com>
2026-01-12 15:54:09 -0700

    last change

--- Week01/scripts/getdates.js
+++ Week01/scripts/getdates.js
@@ -1,4 +1,4 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
-document.getElementById("lastModified").textContent = document.lastModifieds;
+document.getElementById("lastModified").textContent = document.lastModified;
